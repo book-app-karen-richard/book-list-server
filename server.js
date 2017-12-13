5e49c74f5f6e6185ca1bf9f87e359897cc6b43be
kbperez <kbpgperez@hotmail.com>
2017-12-12 19:38:48 -0800

    updates to server.js deployed heroku

--- server.js
+++ server.js
@@ -15,17 +15,18 @@ client.on('error', err => console.error(err));
 app.use(cors());
 
 app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
-app.get('/api/v1/books', (request, response) => {
+
+app.get('/api/v1/books', (req, res) => {
   client.query(`
     SELECT book_id, title, author, image_url FROM books`
   )
-  .then(result => response.send(result.rows))
+  .then(result => res.send(result.rows))
   .catch(console.error);
 });
 
 
 
-
+app.get('*', (req, res) => res.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
 // export PORT=3000
