77722ce0cf29d99b803e0479dd1582087deb0b04
kbperez <kbpgperez@hotmail.com>
2017-12-14 11:33:53 -0800

    working on post

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.static('./book-list-server'));
 //app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
 
 app.get('/api/v1/books', (req, res) => {
-  client.query(`SELECT book_id, title, author, image_url, isban FROM books;`)
+  client.query(`SELECT book_id, title, author, image_url, isbn FROM books;`)
   .then(results => res.send(results.rows))
   .catch(console.error);
 });
@@ -31,12 +31,16 @@ app.get('/api/v1/books', (req, res) => {
 });
 
 app.post('api/v1/books', (req, res) => {
-  client.query(`INSERT INTO books(title, author, isbn, image_url, description) VALUES ($1, $2, $3, $4, $5)`)
+  client.query(`INSERT INTO books(title, author, isbn, image_url, description) VALUES ($1, $2, $3, $4, $5)`
   [ req.body.title,
-    req.bodyauthor,
+    req.body.author,
     req.body.isbn,
     req.body.image_url,
-    req.body.description]
+    req.body.description
+  ])
+  .then(() => response.send('Update complete'))
+  .catch(console.error);
+});
 
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
