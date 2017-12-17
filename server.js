0915ee983392ed12ce165ca27b00ab9febf668bc
Richard Montgomery <rikermonty@gmail.com>
2017-12-16 20:49:07 -0800

    Checkpoint..

--- server.js
+++ server.js
@@ -3,7 +3,8 @@
 const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
-const bodyParser = require('body-parser').urlencoded({extended: true});
+const page = require('page');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT;
 const CLIENT_URL = process.env.CLIENT_URL;
@@ -13,11 +14,11 @@ client.connect();
 client.on('error', err => console.error(err));
 
 app.use(cors());
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: true}));
-// app.use(express.static('./book-list-server'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('./book-list-server'));
 
-
+//Querying the database for books
 app.get('/api/v1/books', (req, res) => {
   client.query(`SELECT book_id, title, author, image_url, isbn FROM books;`)
   .then(results => res.send(results.rows))
@@ -25,7 +26,6 @@ app.get('/api/v1/books', (req, res) => {
 });
 
 app.get('/api/v1/books/:book_id', (req, res) => {
-
   client.query(`SELECT * FROM books WHERE book_id=${req.params.book_id}`)
   .then(results => res.send(results.rows))
   .catch(console.error);
@@ -36,12 +36,11 @@ app.post('/api/v1/books', bodyParser, (req, res) => {
   let {title, author, isbn, image_url, description} = req.body;
   client.query(`
     INSERT INTO books(title, author, isbn, image_url, description) VALUES($1, $2, $3, $4, $5)`,
-  [ title, author, isbn, image_url, description]
+  [title, author, isbn, image_url, description]
 )
   .then(() => res.sendStatus(201))
   .catch(console.error);
 });
-
 
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
